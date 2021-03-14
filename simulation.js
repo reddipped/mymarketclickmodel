e8dbc6b5b658365a1d7ef1ebb65daa66acee5bbb
Peter van Nes <mailbox@petervannes.nl>
2021-03-14 15:58:48 +0100

    article list starter

--- simulation.js
+++ simulation.js
@@ -1,30 +1,72 @@
-//let showLogin = true;
-let showMyOrders = false;
-let showOrders = false;
+// //let showLogin = true;
+// let showMyOrders = false;
+// let showOrders = false;
+(function () {
+  window.addEventListener("load", update, false);
+})();
 
-// Only one should be true 
-let LoginPanel = true ;
-let MyOrder = false ;
-let MySearch = false ;
-let MySettings = false ;
+let errorVisible = false;
+let loginVisible = false;
+let registreerVisible = true;
+let articleListVisible = false ;
 
-function clickMyOrder() {
-    showMyOrders != showMyOrders;
+function update() {
+  show("error", errorVisible);
+  show("login", loginVisible);
+  show("registreer", registreerVisible);
+  show("articlelist", articleListVisible);
+}
+
+function clickOn(event, component) {
+  event.preventDefault();
+  switch (component) {
+    case "registreer":
+      errorVisible = false;
+      loginVisible = true;
+      registreerVisible = false;
+      break;
+    case "login":
+      errorVisible = true;
+      loginVisible = true;
+      registreerVisible = false;
+      break;
+    case "error":
+        errorVisible = false;
+        loginVisible = false;
+        registreerVisible = false;
+        articleListVisible = true;
+        break;
+      
+      break;
+  }
+
+  update();
+}
+
+function show(className, visible) {
+  if (visible) {
+    document.getElementsByClassName(className)[0].style.display = "";
+  } else {
+    document.getElementsByClassName(className)[0].style.display = "none";
+  }
 }
 
 
-function clickLogin() {
-    console.log('login');
-    // showMyOrders(true);
-    showLogin(false);
-
-}
-
-function showLogin(visible) {
-    
-    if (visible) {
-        document.getElementsByClassName('login')[0].style.visibility = 'visible';
-    } else {
-        document.getElementsByClassName('login')[0].style.visibility = 'hidden';
+document.querySelector("ul").addEventListener("click", (e) => {
+    const li = e.target.closest("li");
+    const btn = e.target.closest("button");
+    if (li && li.scrollLeft === 0) {
+      li.scrollBy({
+        left: 1,
+        behavior: "smooth"
+      });
+    } else if (!btn && li) {
+      li.scrollBy({
+        left: -1,
+        behavior: "smooth"
+      });
+    } else if (btn && li) {
+      li.remove();
     }
-}
+  });
+  
