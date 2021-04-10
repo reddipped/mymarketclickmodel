4efd5826851a0b0d87c22051e834dc1109e3a764
Peter van Nes <mailbox@petervannes.nl>
2021-04-10 11:43:08 +0200

    stall done (more or less)

--- simulation.js
+++ simulation.js
@@ -3,13 +3,32 @@
 // let showOrders = false;
 (function () {
   window.addEventListener("load", update, false);
+
+ 
+
 })();
+
+
+$(document).ready(function(){
+
+  console.log('hi mum');
+  $(window).scroll(function() {
+    console.log("window scrol");
+  });
+
+  $("articlelist").scroll(function ()  {
+   console.log("Scorllin");
+ })
+})
+
+
 
 let errorVisible = false;
 let loginVisible = false;
 let registreerVisible = true;
-let articleListVisible = false ;
+let articleListVisible = false;
 let stallVisible = false;
+let article1Visible = false;
 
 function update() {
   show("error", errorVisible);
@@ -17,6 +36,7 @@ function update() {
   show("registreer", registreerVisible);
   show("articlelist", articleListVisible);
   show("stall", stallVisible);
+  show("article1", article1Visible);
 }
 
 function clickOn(event, component) {
@@ -26,36 +46,103 @@ function clickOn(event, component) {
       errorVisible = false;
       loginVisible = true;
       registreerVisible = false;
-      stallVisible = false ;
+      stallVisible = false;
+      article1Visible = false;
       break;
     case "login":
       errorVisible = true;
       loginVisible = true;
       registreerVisible = false;
-      stallVisible = false ;
+      stallVisible = false;
+      article1Visible = false;
       break;
     case "error":
-        errorVisible = false;
-        loginVisible = false;
-        registreerVisible = false;
-        articleListVisible = true;
-        stallVisible = false ;
-        break;
-        case "mystall":
-          errorVisible = false;
-          loginVisible = false;
-          registreerVisible = false;
-          articleListVisible = false;
-          stallVisible = true ;
-          break;
-          
-          case "myorder":
-            errorVisible = false;
-            loginVisible = false;
-            registreerVisible = false;
-            articleListVisible = true;
-            stallVisible = false ;
-            break;
+      errorVisible = false;
+      loginVisible = false;
+      registreerVisible = false;
+      articleListVisible = true;
+      stallVisible = false;
+      article1Visible = false;
+      break;
+    case "mystall":
+      errorVisible = false;
+      loginVisible = false;
+      registreerVisible = false;
+      articleListVisible = false;
+      stallVisible = true;
+      article1Visible = false;
+      break;
+
+    case "myorder":
+      errorVisible = false;
+      loginVisible = false;
+      registreerVisible = false;
+      articleListVisible = true;
+      stallVisible = false;
+      article1Visible = false;
+      break;
+
+    case "article1":
+      errorVisible = false;
+      loginVisible = false;
+      registreerVisible = false;
+      articleListVisible = false;
+      stallVisible = true;
+      article1Visible = true;
+      break;
+
+    case "artpiece":
+      document
+        .getElementsByClassName("artpiece")[0]
+        .classList.remove("articlelist__listitemtextqual--selected");
+      document
+        .getElementsByClassName("artgr")[0]
+        .classList.remove("articlelist__listitemtextqual--selected");
+      document
+        .getElementsByClassName("artkg")[0]
+        .classList.remove("articlelist__listitemtextqual--selected");
+      document
+        .getElementsByClassName("artpiece")[0]
+        .classList.add("articlelist__listitemtextqual--selected");
+        var evt = event ? event : window.event;
+        if (evt.stopPropagation) {evt.stopPropagation();}
+
+      break;
+
+    case "artgr":
+      document
+        .getElementsByClassName("artpiece")[0]
+        .classList.remove("articlelist__listitemtextqual--selected");
+      document
+        .getElementsByClassName("artgr")[0]
+        .classList.remove("articlelist__listitemtextqual--selected");
+      document
+        .getElementsByClassName("artkg")[0]
+        .classList.remove("articlelist__listitemtextqual--selected");
+      document
+        .getElementsByClassName("artgr")[0]
+        .classList.add("articlelist__listitemtextqual--selected");
+        var evt = event ? event : window.event;
+        if (evt.stopPropagation) {evt.stopPropagation();}
+      break;
+
+    case "artkg":
+      document
+        .getElementsByClassName("artpiece")[0]
+        .classList.remove("articlelist__listitemtextqual--selected");
+      document
+        .getElementsByClassName("artgr")[0]
+        .classList.remove("articlelist__listitemtextqual--selected");
+      document
+        .getElementsByClassName("artkg")[0]
+        .classList.remove("articlelist__listitemtextqual--selected");
+      document
+        .getElementsByClassName("artkg")[0]
+        .classList.add("articlelist__listitemtextqual--selected");
+        var evt = event ? event : window.event;
+        if (evt.stopPropagation) {evt.stopPropagation();}
+      break;
+
       break;
   }
 
@@ -70,22 +157,24 @@ function show(className, visible) {
   }
 }
 
+// document.querySelector("ul").addEventListener("click", (e) => {
+//   const li = e.target.closest("li");
+//   const btn = e.target.closest("button");
+//   if (li && li.scrollLeft === 0) {
+//     li.scrollBy({
+//       left: 1,
+//       behavior: "smooth",
+//     });
+//   } else if (!btn && li) {
+//     li.scrollBy({
+//       left: -1,
+//       behavior: "smooth",
+//     });
+//   } else if (btn && li) {
+//     li.remove();
+//   }
+// });
 
-document.querySelector("ul").addEventListener("click", (e) => {
-    const li = e.target.closest("li");
-    const btn = e.target.closest("button");
-    if (li && li.scrollLeft === 0) {
-      li.scrollBy({
-        left: 1,
-        behavior: "smooth"
-      });
-    } else if (!btn && li) {
-      li.scrollBy({
-        left: -1,
-        behavior: "smooth"
-      });
-    } else if (btn && li) {
-      li.remove();
-    }
-  });
-  
+
+
+// $('#my_div1').hasScrollBar()
